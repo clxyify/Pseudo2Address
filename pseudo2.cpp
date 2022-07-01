f13c44ec907a5e0c5e0820218987f3d12bad12e2
clxyify <98536033+clxyify@users.noreply.github.com>
2022-07-01 18:49:33 -0400

    Update pseudo2.cpp

--- pseudo2.cpp
+++ pseudo2.cpp
@@ -1,3 +1,5 @@
+/* IDA dumped version (offsets change once in a week) */
+
 void *__fastcall pseudo2_dumped(DWORD *a1, int a2) /* orignally dumped from IDA */
 {
   int v3;
@@ -34,4 +36,31 @@ void *__fastcall pseudo2_dumped(DWORD *a1, int a2) /* orignally dumped from IDA 
       else
         return (void *)(v3 + 16 * (-10002 - a2 + 1));
   }
+}
+
+/* thanks roblox luau source */
+static LUAU_NOINLINE TValue* pseudo2(lua_State* L, int idx)
+{
+    api_check(L, lua_ispseudo(idx));
+    switch (idx)
+    { /* pseudo-indices */
+    case LUA_REGISTRYINDEX:
+        return registry(L);
+    case LUA_ENVIRONINDEX:
+    {
+        sethvalue(L, &L->global->pseudotemp, getcurrenv(L));
+        return &L->global->pseudotemp;
+    }
+    case LUA_GLOBALSINDEX:
+    {
+        sethvalue(L, &L->global->pseudotemp, L->gt);
+        return &L->global->pseudotemp;
+    }
+    default:
+    {
+        Closure* func = curr_func(L);
+        idx = LUA_GLOBALSINDEX - idx;
+        return (idx <= func->nupvalues) ? &func->c.upvals[idx - 1] : cast_to(TValue*, luaO_nilobject);
+    }
+    }
 }
